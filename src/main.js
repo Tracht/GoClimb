8a0b7c09f581cd5c39b11df1f2dd873bd9bac9cb
Tracht <marija.trachtenberg@gmail.com>
2021-02-06 19:43:00 +0000

    Uses internationalisation (locale) for ElementUI

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import './registerServiceWorker';
@@ -9,6 +10,7 @@ import store from './store';
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(ElementUI, { locale })
 
 new Vue({
   router,
